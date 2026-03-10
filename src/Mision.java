dde46454d102c460882c925d4f9b10d33810336c
Victor Segura <vseguranes@gmail.com>
2026-03-10 08:55:03 +0100

    feat(Mision): método constructor de la clase Mision

--- src/Mision.java
+++ src/Mision.java
@@ -1,2 +1,31 @@
 public class Mision {
-}.
+
+    // atributos
+    private int id;
+    private String nombre;
+    private String dificultad;
+    private int nivelRecomendado;
+    private int recompensaExperiencia;
+    private boolean completada;
+
+    /**
+     * Metodo constructor de la clase Mision
+     * @param id
+     * @param nombre
+     * @param dificultad
+     * @param nivelRecomendado
+     * @param recompensaExperiencia
+     * @param completada
+     */
+    public Mision(int id, String nombre, String dificultad, int nivelRecomendado, int recompensaExperiencia, boolean completada) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dificultad = dificultad;
+        this.nivelRecomendado = nivelRecomendado;
+        this.recompensaExperiencia = recompensaExperiencia;
+        this.completada = completada;
+    }
+
+
+
+}
