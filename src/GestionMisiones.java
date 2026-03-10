472d26a13f9d70aa23a654188ce497fc2011cc1c
Victor Segura <vseguranes@gmail.com>
2026-03-10 09:33:30 +0100

    feat(GestionMisiones): métodos crearMision y agregarMision

--- src/GestionMisiones.java
+++ src/GestionMisiones.java
@@ -13,5 +13,33 @@ public class GestionMisiones {
         this.misiones = new ArrayList<>();
     }
 
+    /**
+     * Devuelve el id de la nueva misión creada
+     *
+     * @param id
+     * @param nombre
+     * @param dificultad
+     * @param nivelRecomendado
+     * @param recompensaExperiencia
+     * @param completada
+     * @return id de la misión creada
+     */
+    public int crearMision(int id, String nombre, String dificultad, int nivelRecomendado, int recompensaExperiencia, boolean completada){
 
+        int idMisionCreada;
+
+        //  creamos la misión
+        Mision mision = new Mision(id, nombre, dificultad, nivelRecomendado, recompensaExperiencia, completada);
+
+        //  agregamos la misión
+        idMisionCreada = agregarMision(mision);
+
+        return idMisionCreada;
+    }
+
+    public int agregarMision(Mision mision){
+        this.misiones.add(mision);
+
+        return mision.getId();
+    }
 }
