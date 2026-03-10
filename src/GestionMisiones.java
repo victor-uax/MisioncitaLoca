a7ce792d8801e5a895a622f8da11444fc64cdfa1
Victor Segura <vseguranes@gmail.com>
2026-03-10 09:03:43 +0100

    feat(GestionMisiones): método constructor de la clase GestionMisiones

--- src/GestionMisiones.java
+++ src/GestionMisiones.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class GestionMisiones {
+
+    // atributos
+    private ArrayList<Mision> misiones;
+
+    /**
+     * Metodo constructor de la clase GestionMisiones
+     * Defino el ArrayList misiones
+     */
+    public GestionMisiones() {
+        this.misiones = new ArrayList<>();
+    }
+
+
 }
